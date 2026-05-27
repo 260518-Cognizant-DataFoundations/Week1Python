d174b193d1ec21867d1edb29e51ccee14feef713
benp3837 <petruzziellbj@alumni.vcu.edu>
2026-05-27 11:01:43 -0400

    initial commit of omakase

---
+++ Omakase/app/main.py
@@ -0,0 +1,12 @@
+""" Omakase is a Japanese dining experience
+where the chef selects and prepares a series of dishes for the customer.
+The word "omakase" translates to "I'll leave it up to you".
+
+
+We'll be creating an Omakase Command Line Interface (CLI) app that demonstrates:
+1. User input and app output
+2. Data structures (lists, dictionaries)
+3. Control flow (review in a more realistic setting)
+4. Functions (to organize/declutter our code and make it more reusable)
+"""
+
