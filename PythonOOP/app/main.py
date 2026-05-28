e60095a3df55dff468de50345ea7746727f74f08
benp3837 <petruzziellbj@alumni.vcu.edu>
2026-05-28 11:00:14 -0400

    instantiated person class twice - we got 2 objects!

--- PythonOOP/app/main.py
+++ PythonOOP/app/main.py
@@ -6,3 +6,20 @@ We'll see Inheritance and Polymorphism in our Person and Pirate Classes
 Then we'll explore Encapsulation and Abstraction with our Ship Class
 """
 
+# Person lives in a different file/package, so we need to import it
+from app.models.person import Person
+
+print("======================(Using the Person Class)")
+
+# Instantiating a Person object (creating a Person object)
+person1 = Person("Ben", 28)
+
+# Invoke the methods of the Person class via the person1 object
+print(person1.say_hello())
+print(person1.eat_food("Pizza"))
+
+# Instantiate ANOTHER Person object with different attributes
+person2 = Person("Sophie", 25)
+
+print(person2.say_hello())
+print(person2.eat_food("Salad"))
